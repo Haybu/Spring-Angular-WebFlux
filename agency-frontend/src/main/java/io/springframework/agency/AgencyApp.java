e6a8f2fa65f703b1a0982e2a669f5b57abb6d58f
Haytham Mohamed <haybu@hotmail.com>
2018-09-20 16:00:34 -0500

    first checkin

---
+++ agency-frontend/src/main/java/io/springframework/agency/AgencyApp.java
@@ -0,0 +1,27 @@
+package io.springframework.agency;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication
+public class AgencyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyApp.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
+		return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
+	}
+}
