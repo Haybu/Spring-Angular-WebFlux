84e26d4cae82db85245db1dc389b57f982471d00
Haytham Mohamed <haybu@hotmail.com>
2018-09-20 16:42:27 -0500

    changed package

--- reservations-service/src/main/java/io/springframework/reservation/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package io.springframework.reservation;
-
-import io.springframework.reservation.entities.Reservation;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ReservationRepository extends ReactiveCrudRepository<Reservation, Long> {
-
-}
